d31c3c2e009ea8ba7c12a1cd924c9e245ac60988
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-29 23:39:50 -0400

    QUick spelling error of html route fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ var path = require("path");
 function htmlRoutes(app) {
 
     app.get("/survey", function(req,res){
-        res.sendFile(path.join(__dirname, "../public/survey.hitml"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
 
     });
     //wildcard for redirecting everything back to the home page
