8c1403e0b4bf71b613bbea7958f3cb30a050cb42
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-29 23:38:45 -0400

    Quick logic of html routing.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+var path = require("path");
+function htmlRoutes(app) {
+
+    app.get("/survey", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/survey.hitml"));
+
+    });
+    //wildcard for redirecting everything back to the home page
+    app.get("*", function(req,res) {
+        res.sendFile(path.join(__dirname, "..public/home.html"));
+    });
+};
+
+module.exports =htmlRoutes;
